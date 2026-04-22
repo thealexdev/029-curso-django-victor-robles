9783344ed9cd6ea930a19ed0ad047e0f70ffa064
thealexdev <axxving@gmail.com>
2026-04-22 17:23:51 -0600

    recomendaciones

---
+++ 01-python/funciones/curiosidades.py
@@ -0,0 +1,10 @@
+"""
+- Las funsiones es resomendable definirlas siempre hasta la parte superior
+- Las funsiones siempre deben de devolver un dato
+- Tener cuidado con el orden en donde se definen cada uno de los bloques de código
+"""
+
+
+def mi_funsion():
+    print('Hola que tal')
+
