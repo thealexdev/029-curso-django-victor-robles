394f6726dc7bb1c2a79e41693ff59a8825368abd
thealexdev <axxving@gmail.com>
2026-04-22 21:40:26 -0600

    listas multidimensionales

---
+++ 01-python/listas/listas-multidimensionales.py
@@ -0,0 +1,16 @@
+"""
+Listas multidimensionales
+- son listas que contienen dentro otras listas
+"""
+
+contactos = [
+    ["Antonio", "antonio@correo.com"],
+    ["Luis", "luis@correo.com"],
+    ["Andres", "andres@correo.com"],
+]
+
+# print(contactos[0][1])
+
+for contacto in contactos:
+    print(contacto[1])
+
