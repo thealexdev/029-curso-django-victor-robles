243743c352939e58e2028678f9db1c4851fb6096
thealexdev <axxving@gmail.com>
2026-05-19 22:51:35 -0600

    modulo de matematicas

---
+++ 01-python/modulos-paquetes/modulo-matematicas.py
@@ -0,0 +1,15 @@
+# Módulo de operaciones matematicas
+import math
+
+print("Raiz cuadrada de 10: ", math.sqrt(10))
+
+print("Número PI: ", float(math.pi))
+
+print("Redondear: ", math.ceil(6.3535353))
+
+print("Redondear: ", math.floor(7.45))
+
+# Modulo random
+import random
+
+print("Número aleatorio entre 15 y 67: ", random.randint(15, 67))
