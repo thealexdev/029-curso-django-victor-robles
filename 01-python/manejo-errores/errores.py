95a72e8b909f39e6422e14c1fea7e186ff2c4d77
thealexdev <axxving@gmail.com>
2026-05-24 14:44:59 -0600

    manejo de errores con try except

---
+++ 01-python/manejo-errores/errores.py
@@ -0,0 +1,18 @@
+"""
+
+Capturar excepciones y manejar errores en código susceptible a fallos/errores
+
+"""
+
+try:
+    nombre = input("Cual es tu nombre?: ")
+
+    if len(nombre) > 1:
+        nombre_usuario = "El nombre es: " + nombre
+    print(nombre_usuario)
+except:
+    print("Ha ocurrido un error, ingresa un nombre valido")
+else:
+    print("Todo ha funcionado correctamente")
+finally:
+    print("Fin de la operación")
