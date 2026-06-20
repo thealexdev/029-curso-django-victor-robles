1a19ec716a8882731ca4f404248a2a3d419b746b
thealexdev <axxving@gmail.com>
2026-06-20 00:34:31 -0500

    constructor

---
+++ 02-python-POO/constructor/coche.py
@@ -0,0 +1,56 @@
+class Coche:
+    # Propiedades
+    color: "Azul"
+    marca: "Nizan"
+    modelo: "2026"
+    velocidad: 300
+    plazas: 2
+
+    # constructor
+    def __init__(self, color, marca, modelo, velocidad, plazas):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+        self.velocidad = velocidad
+        self.plazas = plazas
+
+    # Metodos
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
+
+    def setMarca(self, marca):
+        self.marca = marca
+
+    def getMarca(self):
+        return self.marca
+
+    def setModelo(self, modelo):
+        self.modelo = modelo
+
+    def getModelo(self):
+        return self.modelo
+
+    def setVelocidad(self, velocidad):
+        self.velocidad = self.velocidad
+
+    def getVelocidad(self):
+        return self.velocidad
+
+    def setPlazas(self, plazas):
+        self.plazas = plazas
+
+    def getPlazas(self):
+        return self.plazas
+
+    def getInfo(self):
+        info = "\n ---- Información del coche ----"
+        info += "\n Color: " + self.getColor()
+        info += "\n Marca: " + self.getMarca()
+        info += "\n Modelo: " + self.getModelo()
+        info += "\n Velocidad: " + str(self.getVelocidad())
+        info += "\n Plazas: " + str(self.getPlazas())
+
+        return info
---
+++ 02-python-POO/constructor/constructor.py
@@ -0,0 +1,21 @@
+from coche import Coche
+
+# class Coche(
+#  color: Any,
+#  marca: Any,
+#  modelo: Any,
+#  velocidad: Any,
+#  plazas: Any
+# )
+carro01 = Coche("Rojo", "Nissan", "2026", 300, 5)
+
+carro02 = Coche("Azul", "Toyota", "2025", 250, 5)
+carro03 = Coche("Negro", "Ford", "2024", 280, 4)
+carro04 = Coche("Blanco", "Honda", "2026", 260, 5)
+carro05 = Coche("Gris", "Chevrolet", "2023", 240, 7)
+
+print({carro01.getInfo()})
+print({carro03.getInfo()})
+print({carro02.getInfo()})
+print({carro04.getInfo()})
+print({carro05.getInfo()})
