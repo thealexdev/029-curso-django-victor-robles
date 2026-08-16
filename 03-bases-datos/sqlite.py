26352937fbe9758c0357884927ef0f7859a774cf
thealexdev <axxving@gmail.com>
2026-08-16 13:58:46 -0600

    consultar registros de la bd

--- 03-bases-datos/sqlite.py
+++ 03-bases-datos/sqlite.py
@@ -19,5 +19,30 @@ cursor.execute(
 # guardar cambios
 conexion.commit()
 
+# # insertar datos
+cursor.execute(
+    "INSERT INTO productos (titulo, descripcion, precio) VALUES ('primer producto', 'descripcion de mi producto', 550)"
+)
+# guardar
+conexion.commit()
+
+# listar datos
+cursor.execute("SELECT * FROM productos;")
+
+# obtener los datos en forma de tupla
+productos = cursor.fetchall()
+
+# imprimimos el resultado
+for producto in productos:
+    print(producto)
+
+
+# consultar solo el nombre
+cursor.execute("SELECT titulo FROM productos;")
+
+# sacar el primer registro
+producto = cursor.fetchone()
+print(producto)
+
 # cerrar conexion
 conexion.close()
