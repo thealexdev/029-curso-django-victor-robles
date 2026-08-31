abbaf77cdb1f3fbd08c663cfa7ff641affb6d22d
thealexdev <axxving@gmail.com>
2026-08-31 13:42:24 -0600

    creacion de base de datos

---
+++ 04-app-consola/main.py
@@ -0,0 +1,10 @@
+"""
+Proyecto Python y MySQL
+
+- Abrir asistente
+- Login o Registro
+- Si elegimos registro, creara un usuario en la bbdd
+- Si elegimos logn, identificara al usuario y nos preguntara
+- Crear nota, mostrar notas, borrar nota
+
+"""
