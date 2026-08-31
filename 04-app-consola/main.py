22c97a9b869350316dc4ba04712b0644a66d07d7
thealexdev <axxving@gmail.com>
2026-08-31 14:01:47 -0600

    primeras preguntas en el asistente

--- 04-app-consola/main.py
+++ 04-app-consola/main.py
@@ -8,3 +8,19 @@ Proyecto Python y MySQL
 - Crear nota, mostrar notas, borrar nota
 
 """
+
+print(
+    """
+Acciones disponibles:
+    - regisrto = 0
+    - login    = 1
+
+"""
+)
+
+accion = int(input("Que quieres hacer?: "))
+
+if accion == 0:
+    print("Vamos a realizar el registro")
+elif accion == 1:
+    print("Ingresa tus credenciales para continuar")
