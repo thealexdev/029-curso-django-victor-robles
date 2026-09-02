b17b17730d6f234b53c34aa301aefa201022d263
thealexdev <axxving@gmail.com>
2026-09-02 01:16:03 -0600

    creando modelo y clase

---
+++ 04-app-consola/usuarios/usuario.py
@@ -0,0 +1,13 @@
+class Usuario:
+
+    def __init__(self, nombre, apellidos, email, password):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.email = email
+        self.password = password
+
+    def registrar(self):
+        return self.nombre
+
+    def identificar(self):
+        return self.nombre
